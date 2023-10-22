9bd3d94a967e219b1991780740b43b456cee8555
panda9903 <satwik9903@gmail.com>
2023-10-22 17:11:55 +0530

    Change Favicon and Title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Lower from "./components/Lower";
 import MadeWithLove from "./components/MadeWithLove";
 import Upper from "./components/Upper";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "AlgoZenith";
+  }, []);
+
   return (
     <div className="scroll-smooth">
       <Upper />
