dc64a03a33833e45ded4dccdbe8b598caf3eaf04
panda9903 <satwik9903@gmail.com>
2023-10-22 20:06:30 +0530

    Change Credit bar

--- src/components/MadeWithLove.js
+++ src/components/MadeWithLove.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MadeWithLove = () => {
   return (
-    <div className="flex items-center justify-evenly bg-black text-white">
+    <div className="flex items-center justify-evenly bg-gradient-to-r from-[#033246] to-[#094863] text-white">
       <p className=" text-center">Made with ❤️ using React and TailwindCSS.</p>
       <a href="https://github.com/panda9903/algozenith" target="_blank ">
         {" "}
