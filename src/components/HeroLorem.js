989570b1ab2b4c5302303f86c4192d75e7f28e55
panda9903 <satwik9903@gmail.com>
2023-10-22 17:04:26 +0530

    Solve paragraph overflow

--- src/components/HeroLorem.js
+++ src/components/HeroLorem.js
@@ -3,7 +3,7 @@ import React from "react";
 const HeroLorem = () => {
   return (
     <div className="rcgn__HeroLorem flex flex-col justify-start items-center md:items-start">
-      <p className="text-white w-[500px] mt-8 text-s text-center md:text-left md:mt-0">
+      <p className="text-white max-w-[500px] mt-8 text-s text-center md:text-left md:mt-0">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua
       </p>
