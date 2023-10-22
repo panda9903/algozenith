d7d0ab6308e60fcf22db912376f81d3e91ef8849
panda9903 <satwik9903@gmail.com>
2023-10-22 19:44:04 +0530

    Complete price type feature

--- src/components/NewPrice.js
+++ src/components/NewPrice.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Typed from "react-typed";
 
 const NewPrice = () => {
   return (
     <p className="rcgn__NewPrice text-[#20588c] text-[2.5rem]  font-extrabold">
-      ₹13,999
+      {/*       ₹13,999
+       */}
+      <Typed strings={["₹13,999"]} typeSpeed={90} backSpeed={50} loop={true} />
     </p>
   );
 };
